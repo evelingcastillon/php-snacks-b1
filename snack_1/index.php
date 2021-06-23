d899ea859a0e61ebe0b9d395df1497f2d7e3aed4
evelingcastillon <evelingcastillon0304@gmail.com>
2021-06-23 12:24:45 +0200

    aggiunto img

--- snack_1/index.php
+++ snack_1/index.php
@@ -1,73 +1,73 @@
 <?php
     #Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario.
-    $partite = [
-        [
-            'squadraCasa' => 'Olimpia Milano',
-            'squadraOspite' => 'Cantù',
-            'puntiCasa' => '55',
-            'puntiOspite' =>'60'
-        ],
-        [
-            'squadraCasa' => 'Fortitudo Bologna',
-            'squadraOspite' => 'Virtus Bologna',
-            'puntiCasa' => '78',
-            'puntiOspite' =>'50'
-        ],
-        [
-            'squadraCasa' => 'Varese',
-            'squadraOspite' => 'Treviso',
-            'puntiCasa' => '75',
-            'puntiOspite' =>'60'
-        ],
-        [
-            'squadraCasa' => 'Stella Azzurra',
-            'squadraOspite' => 'Mens Sana Siena',
-            'puntiCasa' => '59',
-            'puntiOspite' =>'80'
-        ],
-        [
-            'squadraCasa' => 'Reggiana',
-            'squadraOspite' => 'Juvecaserta',
-            'puntiCasa' => '65',
-            'puntiOspite' =>'68'
-        ],
-        [
-            'squadraCasa' => 'Scandone Avellino',
-            'squadraOspite' => 'Auxilium Torino',
-            'puntiCasa' => '37',
-            'puntiOspite' =>'64'
-        ],
-        [
-            'squadraCasa' => 'Triestina',
-            'squadraOspite' => 'Libertas Livorno',
-            'puntiCasa' => '68',
-            'puntiOspite' =>'62'
-        ],
-        [
-            'squadraCasa' => 'Vanoli Cremona',
-            'squadraOspite' => 'Petrarca',
-            'puntiCasa' => '72',
-            'puntiOspite' =>'61'
-        ],
-        [
-            'squadraCasa' => 'Dinamo Sassari',
-            'squadraOspite' => 'Partenope',
-            'puntiCasa' => '59',
-            'puntiOspite' =>'63'
-        ],
-        [
-            'squadraCasa' => 'New Basket Brindisi',
-            'squadraOspite' => 'Teramo Basket',
-            'puntiCasa' => '60',
-            'puntiOspite' =>'81'
-        ],
-        [
-            'squadraCasa' => 'Aquila Trento',
-            'squadraOspite' => 'Messina',
-            'puntiCasa' => '75',
-            'puntiOspite' =>'35'
-        ]
+$partite = [
+    [
+        'squadraCasa' => 'Olimpia Milano',
+        'squadraOspite' => 'Cantù',
+        'puntiCasa' => '55',
+        'puntiOspite' =>'60'
+    ],
+    [
+        'squadraCasa' => 'Fortitudo Bologna',
+        'squadraOspite' => 'Virtus Bologna',
+        'puntiCasa' => '78',
+        'puntiOspite' =>'50'
+    ],
+    [
+        'squadraCasa' => 'Varese',
+        'squadraOspite' => 'Treviso',
+        'puntiCasa' => '75',
+        'puntiOspite' =>'60'
+    ],
+    [
+        'squadraCasa' => 'Stella Azzurra',
+        'squadraOspite' => 'Mens Sana Siena',
+        'puntiCasa' => '59',
+        'puntiOspite' =>'80'
+    ],
+    [
+        'squadraCasa' => 'Reggiana',
+        'squadraOspite' => 'Juvecaserta',
+        'puntiCasa' => '65',
+        'puntiOspite' =>'68'
+    ],
+    [
+        'squadraCasa' => 'Scandone Avellino',
+        'squadraOspite' => 'Auxilium Torino',
+        'puntiCasa' => '37',
+        'puntiOspite' =>'64'
+    ],
+    [
+        'squadraCasa' => 'Triestina',
+        'squadraOspite' => 'Libertas Livorno',
+        'puntiCasa' => '68',
+        'puntiOspite' =>'62'
+    ],
+    [
+        'squadraCasa' => 'Vanoli Cremona',
+        'squadraOspite' => 'Petrarca',
+        'puntiCasa' => '72',
+        'puntiOspite' =>'61'
+    ],
+    [
+        'squadraCasa' => 'Dinamo Sassari',
+        'squadraOspite' => 'Partenope',
+        'puntiCasa' => '59',
+        'puntiOspite' =>'63'
+    ],
+    [
+        'squadraCasa' => 'New Basket Brindisi',
+        'squadraOspite' => 'Teramo Basket',
+        'puntiCasa' => '60',
+        'puntiOspite' =>'81'
+    ],
+    [
+        'squadraCasa' => 'Aquila Trento',
+        'squadraOspite' => 'Messina',
+        'puntiCasa' => '75',
+        'puntiOspite' =>'35'
     ]
+    ];
     #Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
     #Stampiamo a schermo tutte le partite con questo schema.
     #Olimpia Milano - Cantù | 55-60
--- snack_2/index.php
+++ snack_2/index.php
@@ -1,6 +1,13 @@
 <?php
 
 #Passare come parametri GET name, mail e age e verificare
+/* $_GET['name'];
+$_GET['mail'];
+$_GET['age']; */
+$pattern = '/.+@.+\.[A-z]*/m';
+if(isset($_GET['name']) && isset($_GET['mail']) && isset($_GET['age'])) {
+    
+}
 #(cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri,
 #che mail contenga un punto e una chiocciola e che age sia un numero.
 #Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
